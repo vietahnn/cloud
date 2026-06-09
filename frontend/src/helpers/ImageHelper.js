57f5cd58ee8ef176562ebd9f003e068feaebaf4a
Howard <thoaihaotang@gmail.com>
2026-06-09 21:48:07 +0700

    config aws

--- frontend/src/helpers/ImageHelper.js
+++ frontend/src/helpers/ImageHelper.js
@@ -1,5 +1,9 @@
 import { API_IMAGES_BASE_URL } from "../config/config";
 
 export function getImageURL(path) {
+    if (!path) return null;
+    if (path.startsWith("http://") || path.startsWith("https://")) {
+        return path;
+    }
     return API_IMAGES_BASE_URL + path;
 }
